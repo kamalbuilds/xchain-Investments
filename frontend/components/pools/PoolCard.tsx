7fed7dc9c1299f26ef4d21d2f1aa9e9ff8e7a396
Abhijeet1520 <abhijeetkumar12346@gmail.com>
2024-09-22 00:35:30 +1000

    Update PoolCard to reflect pool terminology and use Pool interface

--- frontend/components/pools/PoolCard.tsx
+++ frontend/components/pools/PoolCard.tsx
@@ -1,5 +1,3 @@
-import * as React from "react"
-
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -11,20 +9,21 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Pool } from "@/interfaces"
 
-const PoolCard = ({ pool }: { pool: any }) => {
+const PoolCard = ({ pool }: { pool: Pool }) => {
   return (
     <Card className="w-[450px]">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>Create pool</CardTitle>
+        <CardDescription>Deploy your new pool in one-click.</CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
+              <Input id="name" placeholder="Name of your pool" />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="framework">Framework</Label>
